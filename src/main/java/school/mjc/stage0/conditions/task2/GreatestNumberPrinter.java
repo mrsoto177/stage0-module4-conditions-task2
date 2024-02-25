38e6d2005b4edfc68859f9487ce2e4c04bfe50f3
Marcos <msoto@hopewellsystem.com>
2024-02-24 19:18:19 -0600

    m4 t2 cond 1

--- src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
@@ -2,5 +2,20 @@ package school.mjc.stage0.conditions.task2;
 
 public class GreatestNumberPrinter {
     public void printGreatest(int first, int second) {
+    	
+    	if (first > second) {
+    		System.out.println(first);
+    	}else {
+    		System.out.println(second);
+    	}
+    	
+    	
+    }
+    
+    public static void main(String[] args) {
+    	GreatestNumberPrinter comp = new GreatestNumberPrinter ();
+    	int first = 38;
+    	int second = 39;
+    	comp.printGreatest(first, second);
     }
 }
